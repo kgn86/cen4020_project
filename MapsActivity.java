adb444bc9b87f42a385c5a25a0583e5a8eeaaeff
AndresG0nzalez <43159534+AndresG0nzalez@users.noreply.github.com>
2019-11-15 16:27:50 -0500

    Add files via upload
    Added garage locations and map marker variables

--- MapsActivity.java
+++ MapsActivity.java
@@ -19,16 +19,46 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
+    private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    //Garages
+    private static final LatLng SPIRIT = new LatLng(30.443521, -84.305285);
+    private static final LatLng PENSACOLA = new LatLng(30.438014, -84.299836);
+    private static final LatLng CALL_STREET = new LatLng(30.444050, -84.289161);
+    private static final LatLng SAINT_AUGUSTINE = new LatLng(30.437705, -84.290161);
+    private static final LatLng TRADITIONS = new LatLng(30.441931, -84.297074);
+    private static final LatLng WOODWARD = new LatLng(30.444836, -84.298962);
+    //Markers
+    private Marker mSpirit;
+    private Marker mPensacola;
+    private Marker mCall_St;
+    private Marker mSt_Aug;
+    private Marker mTraditions;
+    private Marker mWoodward;
+
+    //widgets
+    //private EditText mSearchText;
+
+    //variables
+    private GoogleMap mMap;
+    private boolean LocationPermissionGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onMapReady: Map is ready");
+        Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
         if(LocationPermissionGranted) {
@@ -39,29 +69,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
-
             mMap.setMyLocationEnabled(true);
-
+            //mMap.getUiSettings().setMyLocationButtonEnabled(false); //disable the current location btn at the top right
+            //init();
 
         }
-    }
-    private static final String TAG = "MapsActivity";
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private static final float DEFAULT_ZOOM = 15f;
+        mSpirit = mMap.addMarker(new MarkerOptions()
+                .position(SPIRIT)
+                .title("Spirit Garage"));
+        mSpirit.setTag(0);
 
-    //variables
-    private GoogleMap mMap;
-    private boolean LocationPermissionGranted = false;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
+        mCall_St = mMap.addMarker(new MarkerOptions()
+                .position(CALL_STREET)
+                .title("Call Street Garage"));
+        mCall_St.setTag(0);
+
+        mPensacola = mMap.addMarker(new MarkerOptions()
+                .position(PENSACOLA)
+                .title("West Pensacola Street Garage"));
+        mPensacola.setTag(0);
+
+        mSt_Aug = mMap.addMarker(new MarkerOptions()
+                .position(SAINT_AUGUSTINE)
+                .title("St Augustine Garage"));
+        mSt_Aug.setTag(0);
+
+        mTraditions = mMap.addMarker(new MarkerOptions()
+                .position(TRADITIONS)
+                .title("Traditions Way Garage"));
+        mTraditions.setTag(0);
+
+        mWoodward = mMap.addMarker(new MarkerOptions()
+                .position(WOODWARD)
+                .title("Woodward Garage"));
+        mWoodward.setTag(0);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        //mSearchText = (EditText) findViewById(R.id.input_search);
         getLocationPermission();
+
     }
+
+    /*private void init(){
+        Log.d(TAG, "init: initializing");
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                || actionId == EditorInfo.IME_ACTION_DONE
+                || event.getAction() == KeyEvent.ACTION_DOWN
+                || event.getAction() == KeyEvent.KEYCODE_ENTER){
+                //execute our search method
+                //geoLocate();
+                }
+
+                return false;
+            }
+        });
+    }*/
+
+    /*private void geoLocate(){
+        Log.d(TAG, "geoLocate: geolocating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
+
+
+        }
+    }*/
 
     private void getDeviceLocation(){
     Log.d(TAG, "getDeviceLocation: getting the devices current location");
@@ -78,7 +175,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             Location currentLocation = (Location) task.getResult();
 
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
-                                    DEFAULT_ZOOM);
+                                    DEFAULT_ZOOM, "My Location");
 
                         } else{
                             Log.d(TAG, "onComplete: current location is null");
@@ -94,7 +191,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    private void moveCamera(LatLng latlng, float zoom){
+    private void moveCamera(LatLng latlng, float zoom, String title){
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latlng.latitude + ", lng: " + latlng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
     }
@@ -147,5 +244,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 }
