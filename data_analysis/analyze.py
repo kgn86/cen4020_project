ba0bc444a4be138ee94b5dce29a6efd07c8d899b
kgn86 <kwebsterfl@gmail.com>
2019-11-17 21:07:17 -0500

    simplified analysis, now outputs to a json

--- data_analysis/analyze.py
+++ data_analysis/analyze.py
@@ -6,8 +6,7 @@ from datetime import datetime
 from time import strptime, mktime 
 from collections import namedtuple
 
-occ_pt      = namedtuple('occ_pt', 'pt span')
-garage_data = namedtuple('garage_data', 'last pts')
+occ_pt      = namedtuple('occ_pt', 'pt t')
 
 
 def main():
@@ -15,7 +14,24 @@ def main():
 
     collect(garages)
     merge_pts(garages)
+    print(output_json(garages))
 
+
+def num_pts(garages):
+    c = 0
+
+    for g in garages.values():
+        c += len(g.pts)
+
+    return c
+
+
+def print_pts(garages):
+    for g in garages:
+        print('iterating %s' % g)
+        for pt in garages[g]:
+            print('pt: %d\ttype:%s' % (pt.pt, pt.t))
+        return
 
 def collect(garages):
     for root, dirs, files in os.walk('./sample'):
@@ -25,44 +41,55 @@ def collect(garages):
             j = json.loads(d)
             
             for item in j['occupancy2']:
-                cur_occ = item['occupied']
-                name    = item['name']
-                    
-                if name not in garages:
-                    garages[name] = garage_data(last=cur_occ, pts=[])
-                else:
-                    cap = item['capacity']
-                    
-                    if (garages[name].last == cap and cur_occ != cap) or (cur_occ <= cap * 0.85):
-                        ts = item['datestamp']
-                        epoch_ts = int(mktime(strptime(ts, '%Y-%m-%dT%H:%M:%S')))
+                if item['factype'] != 'W':
+                    continue
 
-                        pt = occ_pt(pt=epoch_ts, span=5*60)
-                        garages[name].pts.append(pt)
-                        
-                garages[name]._replace(last=cur_occ)
+                cur_occ = item['occupied']
+                cap     = item['capacity']
+                name    = item['name']
+
+                def ins_pt(t: str):
+                    epoch_time = int(mktime(strptime(item['datestamp'], '%Y-%m-%dT%H:%M:%S')))
+                    pt = occ_pt(pt=epoch_time, t=t)
+                    garages[name].append(pt)
+
+                if name not in garages:
+                    garages[name] = []
+                else:
+                    if cur_occ >= cap * 0.99:
+                        ins_pt('max')
+                    elif cur_occ >= cap * 0.95:
+                        ins_pt('high')
+                    elif cur_occ >= cap * 0.80:
+                        ins_pt('med')
+                    else:
+                        ins_pt('low')
     return
 
 
-def merge_pts(g):
+def merge_pts(garages):
     for g in garages.values():
-        while i < itr_max:
-            cur_pt = g.pts[i]
-            nxt_pt = g.pts[i + 1]
-            
-            overlap_pt = cur_pt.pt + cur_pt.span
-            if overlap_pt >= nxt_pt.pt:
-                new_span = cur_pt.span + nxt_pt.span
-                g.pts[i]._replace(pt=overlap_pt, span=new_span)
-                g.pts.pop(i)
+        i = 0
+        itr_max = len(g) - 1
 
+        while i < itr_max:
+            cur = g[i]
+            nxt = g[i + 1]
+            
+            if cur.t == nxt.t:
+                g.pop(i + 1)
                 itr_max -= 1
             else:
                 i += 1
-            
-
-
+ 
     return
+
+def output_json(garages):
+    j = {}
+    for name in garages:
+        j[name] = garages[name]
+
+    return json.dumps(j, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
     main()
