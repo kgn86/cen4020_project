3e63f3bad78a73812d6e0031883367767fd3ecc2
AndresG0nzalez <43159534+AndresG0nzalez@users.noreply.github.com>
2019-12-01 10:53:56 -0500

    Add files via upload
    Added custom map markers to map fragment

--- MapsActivity.java
+++ MapsActivity.java
@@ -5,12 +5,16 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -18,13 +22,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MapsActivity extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private static final String TAG = "MapsActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -39,6 +44,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final LatLng SAINT_AUGUSTINE = new LatLng(30.437705, -84.290161);
     private static final LatLng TRADITIONS = new LatLng(30.441931, -84.297074);
     private static final LatLng WOODWARD = new LatLng(30.444836, -84.298962);
+
     //Markers
     private Marker mSpirit;
     private Marker mPensacola;
@@ -47,123 +53,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Marker mTraditions;
     private Marker mWoodward;
 
-    //widgets
-    //private EditText mSearchText;
-
     //variables
+    private MapViewModel mapViewModel;
     private GoogleMap mMap;
     private boolean LocationPermissionGranted = false;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        mapViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.activity_maps, container, false);
+        getLocationPermission();
+        return root;
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "Map is ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
         if(LocationPermissionGranted) {
             getDeviceLocation();
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(),
                     Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
             mMap.setMyLocationEnabled(true);
-            //mMap.getUiSettings().setMyLocationButtonEnabled(false); //disable the current location btn at the top right
-            //init();
-
         }
+        //initialize markers on map fragment
+        //Customize map markers with image of garage and name a the top of marker
         mSpirit = mMap.addMarker(new MarkerOptions()
                 .position(SPIRIT)
-                .title("Spirit Garage"));
+                .title("Spirit Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.spirittag)));
         mSpirit.setTag(0);
 
         mCall_St = mMap.addMarker(new MarkerOptions()
                 .position(CALL_STREET)
-                .title("Call Street Garage"));
+                .title("Call Street Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.callstreettag)));
+
         mCall_St.setTag(0);
 
         mPensacola = mMap.addMarker(new MarkerOptions()
                 .position(PENSACOLA)
-                .title("West Pensacola Street Garage"));
+                .title("West Pensacola Street Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pensacolatag)));
+
         mPensacola.setTag(0);
 
         mSt_Aug = mMap.addMarker(new MarkerOptions()
                 .position(SAINT_AUGUSTINE)
-                .title("St Augustine Garage"));
+                .title("St Augustine Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.staugtag))); //175px
+
         mSt_Aug.setTag(0);
 
         mTraditions = mMap.addMarker(new MarkerOptions()
                 .position(TRADITIONS)
-                .title("Traditions Way Garage"));
+                .title("Traditions Way Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.traditionstag)));
         mTraditions.setTag(0);
 
         mWoodward = mMap.addMarker(new MarkerOptions()
                 .position(WOODWARD)
-                .title("Woodward Garage"));
+                .title("Woodward Garage")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.woodwardtag)));
         mWoodward.setTag(0);
-
     }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        //mSearchText = (EditText) findViewById(R.id.input_search);
-        getLocationPermission();
-
-    }
-
-    /*private void init(){
-        Log.d(TAG, "init: initializing");
-
-        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_SEARCH
-                || actionId == EditorInfo.IME_ACTION_DONE
-                || event.getAction() == KeyEvent.ACTION_DOWN
-                || event.getAction() == KeyEvent.KEYCODE_ENTER){
-                //execute our search method
-                //geoLocate();
-                }
-
-                return false;
-            }
-        });
-    }*/
-
-    /*private void geoLocate(){
-        Log.d(TAG, "geoLocate: geolocating");
-
-        String searchString = mSearchText.getText().toString();
-
-        Geocoder geocoder = new Geocoder(MapsActivity.this);
-        List<Address> list = new ArrayList<>();
-        try{
-            list = geocoder.getFromLocationName(searchString, 1);
-        }catch (IOException e){
-            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
-        }
-
-        if(list.size() > 0){
-            Address address = list.get(0);
-
-            Log.d(TAG, "geoLocate: found a location: " + address.toString());
-            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
-
-            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
-
-
-        }
-    }*/
 
     private void getDeviceLocation(){
     Log.d(TAG, "getDeviceLocation: getting the devices current location");
-    mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
+    mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         try{
             if(LocationPermissionGranted){
                 Task location = mFusedLocationProviderClient.getLastLocation();
@@ -179,18 +150,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                         } else{
                             Log.d(TAG, "onComplete: current location is null");
-                            Toast.makeText(MapsActivity.this,"unable to get current location",
+                            Toast.makeText(getActivity(),"unable to get current location",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
-
         }catch (SecurityException e) {
             Log.d(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
         }
     }
-
     private void moveCamera(LatLng latlng, float zoom, String title){
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latlng.latitude + ", lng: " + latlng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
@@ -198,7 +167,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void initMap() {
         Log.d(TAG, "initMap: initialization of map");
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -207,18 +176,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         Log.d(TAG, "getLocationPermission: getting location permission");
         String[] permissions = {FINE_LOCATION, COURSE_LOCATION};
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+            if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 LocationPermissionGranted = true;
                 initMap();
             } else {
-                ActivityCompat.requestPermissions(this, permissions,
+                ActivityCompat.requestPermissions(getActivity(), permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
         } else {
-            ActivityCompat.requestPermissions(this, permissions,
+            ActivityCompat.requestPermissions(getActivity(), permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
